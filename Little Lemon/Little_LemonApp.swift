dfc1219e5d73a3fd7b428c8f1e58798ffd83401d
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2024-05-21 21:35:01 +0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by nikoloz tsirdava on 21.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
