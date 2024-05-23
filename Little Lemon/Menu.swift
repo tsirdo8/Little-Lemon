e6b8f80eb53c7ce5bf79de4861e950f370d7d591
tsirdo8 <104384778+tsirdo8@users.noreply.github.com>
2024-05-23 22:02:23 +0400

    just saving changes

---
+++ Little Lemon/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  Little Lemon
+//
+//  Created by nikoloz tsirdava on 22.05.24.
+//
+
+import SwiftUI
+
+struct Menu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Menu()
+}
